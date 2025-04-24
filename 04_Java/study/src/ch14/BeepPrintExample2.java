4166e145f37768dd47b8b0388792701ef091f9bd
Roa Kim <roa5108@gmail.com>
2025-04-24 15:13:16 +0900

    0424

---
+++ 04_Java/study/src/ch14/BeepPrintExample2.java
@@ -0,0 +1,34 @@
+package ch14;
+
+import java.awt.*;
+
+public class BeepPrintExample2 {
+
+    public static void main(String[] args) {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                Toolkit toolkit = Toolkit.getDefaultToolkit();
+                for (int
+                     i = 0;
+                     i < 5;
+                     i++) {
+
+                    toolkit.beep();
+                    try {
+                        Thread.sleep(500);
+                    } catch (Exception e) {
+                    }
+                }
+            }
+        };
+        thread.start();
+        for (int i = 0; i < 5; i++) {
+            System.out.println("띵");
+            try {
+                Thread.sleep(500);
+            } catch (Exception e) {
+            }
+        }
+    }
+}
